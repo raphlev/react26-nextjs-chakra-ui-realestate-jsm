82eefe19a6c7bb5e6b16772418327701efd7c204
raphlev <raphleveque@hotmail.fr>
2021-11-28 22:50:00 +0100

    u

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
-      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='blue.600'>{desc1}<br />{desc2}</Text>
       <Button fontSize='xl' bg="blue.300" color="white">
         <Link href={linkName}><a>{buttonText}</a></Link>
       </Button>
